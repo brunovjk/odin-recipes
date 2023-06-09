91e17c94b9559b7ec873df079e883a09987d3d20
brunovjk <brunovjk@gmail.com>
2023-06-09 19:35:31 -0300

    fix: fizz or buzz input array

--- projects/fizz.js
+++ projects/fizz.js
@@ -2,19 +2,25 @@ var form = document.getElementById("formId");
 var output = document.getElementById("outputId");
 
 function submitForm(event) {
-  //Preventing page refresh
+  // Preventing page refresh
   event.preventDefault();
-  const value = event.target.elements[0].value;
-  if (value % 3 === 0 && value % 5 === 0) {
-    output.innerText = "FizzBuzz";
-  } else if (value % 3 === 0) {
-    output.innerText = "Fizz";
-  } else if (value % 5 === 0) {
-    output.innerText = "Buzz";
-  } else {
-    output.innerText = value.toString();
+  const values = event.target.elements[0].value.split(",").map(Number);
+  const result = [];
+
+  for (let value of values) {
+    if (value % 3 === 0 && value % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (value % 3 === 0) {
+      result.push("Fizz");
+    } else if (value % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(value);
+    }
   }
+
+  output.innerText = JSON.stringify(result);
 }
 
-//Calling a function during form submission.
+// Calling the function during form submission.
 form.addEventListener("submit", submitForm);
